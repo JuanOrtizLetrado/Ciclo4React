7943806c9c767bd29a0e77eb810fdadc2e3450d5
unknown <jumortizl@correo.udistrital.edu.co>
2021-11-12 21:47:30 -0500

    ejercicio con contexto de tema oscuro

--- src/App.js
+++ src/App.js
@@ -8,18 +8,27 @@ import Admin from './pages/admin/admin';
 import Layout from './layout/Layout';
 import Layout2 from './layout/Layout2';
 import Usuario from './pages/admin/Usuario';
-
+import {UserContext} from './context/user';
+import {DarkContext, useDark} from './context/dark';
+import { useState } from 'react';
+import Configuracion from './pages/Configuracion';
 
 function App() {
+  const [dark,setDark] = useState(false);
+  const [userData,setUserData] = useState({nombre: "Juan Letrado"});
+  
   return (
+    <DarkContext.Provider value={{dark,setDark}}>
+    <UserContext.Provider value={{userData, setUserData}} >
     <BrowserRouter>
     <Routes>
        
       <Route path="/" element={<Layout2/>}>
       <Route  path="" element={<Index/>}/>
       <Route  path="contacto" element={<Contacto/>}/>
+      <Route  path="configuracion" element={<Configuracion/>}/>
       </Route>
-      
+
 
       <Route path="/admin" element={<Layout/>}>
       <Route  path="" element={<IndexA/>}/>
@@ -29,7 +38,8 @@ function App() {
 
     </Routes>
     </BrowserRouter>
-      
+    </UserContext.Provider> 
+    </DarkContext.Provider>
   );
 }
 
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import { useDark } from '../context/dark';
+import { useUser } from '../context/user'
 
 
 const Index = () => {
+    const {userData} = useUser();
+    const { dark } = useDark();
+
+    useEffect(()=>{
+        console.log('este es un useEfect que se ejecuta una sola vez');
+        console.log('info del usuario', userData);
+    },[]);
     return (
-        <div>
-            Este es el index por referencia
+        <div className={`${dark?'fondo-oscuro':'fondo-claro'}`}>
+           <p className={`${dark?'letra-blanca': 'letra-nigga'}`}> Este es el index por referencia </p>
+            <Link to="/configuracion">
+            Ir a Config
+            </Link>
         </div>
     )
 }
